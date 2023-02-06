bd923be9619fa5c9af1701047880f0c0d49828ff
AlyLoutfy <alyloutfy99@gmail.com>
2023-02-06 16:31:02 +0200

    Add color picker, clear, new grid size

--- script.js
+++ script.js
@@ -1,20 +1,65 @@
+const DEFAULT_COLOR = '#43da85';
+const DEFAULT_SIZE = 16;
+
+let currentColor = DEFAULT_COLOR;
+let currentSize = DEFAULT_SIZE;
+
+function setCurrentColor(newColor) {
+  currentColor = newColor;
+}
+
+function setCurrentSize(newSize) {
+  currentSize = newSize;
+}
+
+const colorPicker = document.querySelector(".color-picker");
+const clearBtn = document.querySelector(".clear-btn");
 const grid = document.querySelector(".grid");
+const gridSize = document.querySelector(".grid-size");
+
+colorPicker.oninput = (e) => setCurrentColor(e.target.value);
+clearBtn.onclick = () => newGrid();
+
+function changeRange(newVal) {
+  gridSize.innerHTML = "Grid Size: "+ newVal + " x "+ newVal;
+  currentSize = newVal;
+  newGrid();
+}
 
 function newGrid() {
-  for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
+  clearGrid();
+  setupGrid(currentSize);
+}
 
-      let child = document.createElement("div");
-      child.classList.add("grid-blocks");
-      grid.append(child);
-    }
+function clearGrid() {
+  grid.innerHTML = "";
+}
+
+function setupGrid(size) {
+  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+  grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+
+  for (let i = 0; i < size ** 2; i++) {
+    const child = document.createElement("div");
+    child.classList.add("grid-blocks");
+    child.addEventListener('mouseover', changeColor);
+    child.addEventListener('mousedown', changeColor);
+    grid.append(child);
+  
   }
 }
 
-
-
+function changeColor() {
+  const gridBlocks = document.querySelectorAll(".grid-blocks");
+  gridBlocks.forEach(block => {
+    block.addEventListener('mousedown', event => {
+      block.style.backgroundColor = currentColor;
+    })
+  });
+}
 
 function init() {
   newGrid();
+  changeColor();
 }
 init();
